d551aaef2feb5f24336e3c397e57004dc741e132
Archie Baer <archie@abaer.dev>
2020-08-11 04:29:36 +0100

    Limits post content length

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,18 +1,26 @@
 import auth from '../../utils/auth'
 import { v4 as uuid } from 'uuid'
 import db from "../../db";
+import config from "../../config";
 
 export default async (req, res) => {
   const user = await auth(req.cookies.sessionToken)
   if (!user) return res.status(401).send({ err: 'badAuthorization' })
-  if (typeof req.body.content !== 'string') return res.status(400).json({ err: 'badRequest' })
+
+  const {content} = req.body;
+
+  if (typeof content !== 'string') return res.status(400).json({err: "badRequest"})
+  if (
+    content.length < 1 ||
+    content.length > config.maxPostLength
+  ) return res.status(400).json({ err: 'micro.post.length' })
 
   // Create post
   const post = await db.Post.create({
     id: uuid(),
     author: user.id,
     bot: false,
-    content: req.body.content,
+    content,
     score: 0
   });
 
