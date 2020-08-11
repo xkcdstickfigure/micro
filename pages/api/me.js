47c7b9797d577561b93c28495c3e93e29f8323d1
Archie Baer <archie@abaer.dev>
2020-08-11 01:58:10 +0100

    Formatting

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,33 +1,33 @@
-import axios from "axios";
-const {NEXUS_ID, NEXUS_SECRET, NEXUS_URI} = process.env;
+import axios from 'axios'
+const { NEXUS_ID, NEXUS_SECRET, NEXUS_URI } = process.env
 
 export default async (req, res) => {
-    const token = req.cookies.sessionToken;
-    if (!token) return res.status(401).send({ err: "badAuthorization" });
+  const token = req.cookies.sessionToken
+  if (!token) return res.status(401).send({ err: 'badAuthorization' })
 
-	try {
-		// Get session from token
-		const session = (await axios.post(`${NEXUS_URI}/sessions/token`, {
-                token
-            }, {
-                auth: {
-                    username: NEXUS_ID,
-                    password: NEXUS_SECRET
-                }
-            }
-        )).data;
+  try {
+    // Get session from token
+    const session = (await axios.post(`${NEXUS_URI}/sessions/token`, {
+      token
+    }, {
+      auth: {
+        username: NEXUS_ID,
+        password: NEXUS_SECRET
+      }
+    }
+    )).data
 
-        // Get user by id
-        const user = (await axios.get(`${NEXUS_URI}/users/${session.user}`, {
-            auth: {
-                username: NEXUS_ID,
-                password: NEXUS_SECRET
-            }
-        })).data;
+    // Get user by id
+    const user = (await axios.get(`${NEXUS_URI}/users/${session.user}`, {
+      auth: {
+        username: NEXUS_ID,
+        password: NEXUS_SECRET
+      }
+    })).data
 
-        // Response
-		res.json(user);
-	} catch (err) {
-		res.status(500).send({ err: "internalError" });
-	}
-};
+    // Response
+    res.json(user)
+  } catch (err) {
+    res.status(500).send({ err: 'internalError' })
+  }
+}
