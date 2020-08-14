6dcf1e8fb97f3797b6d432c8b355cced04f348ab
Archie Baer <archie@abaer.dev>
2020-08-14 04:20:41 +0100

    Shows parent posts

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -12,6 +12,8 @@ export default function PostPage () {
 
   return (
     <Page title={post ? `${post.author.name}#${post.author.tag}: ${post.content.split('\n')[0]}` : null}>
+      {post && post.parent ? <Parent id={post.parent} /> : <></>}
+
       <Post
         id={id}
         onLoad={data => setPost(data)}
@@ -27,5 +29,22 @@ export default function PostPage () {
 
       {post ? post.children.list.map(id => <Post key={id} id={id} />) : <></>}
     </Page>
+  )
+}
+
+function Parent ({ id }) {
+  const [post, setPost] = useState()
+
+  return (
+    <>
+      {post && post.parent ? <Parent id={post.parent} /> : <></>}
+      <Post
+        id={id}
+        onLoad={data => setPost(data)}
+      />
+      <div>
+        <div className='mx-auto -my-7 w-1 h-7 bg-primary' />
+      </div>
+    </>
   )
 }
