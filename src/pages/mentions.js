8ded10e343c0cf1d0a492590fdeade5193a18df6
Archie Baer <archie@abaer.dev>
2020-08-15 19:23:24 +0100

    Breadcrumbs on mentions page

--- src/pages/mentions.js
+++ src/pages/mentions.js
@@ -2,13 +2,17 @@ import Page from '../components/Page'
 import Post from '../components/Post'
 import axios from 'axios'
 import { useEffect } from 'react'
+import { Breadcrumb } from '@reactants/ui'
 
 const Mentions = ({ posts }) => {
   // Mark as read
   useEffect(() => { axios.post('/api/mentions/read') }, [])
 
   return (
-    <Page title='Mentions and Replies'>
+    <Page
+      title='Mentions and Replies'
+      breadcrumbs={<Breadcrumb.Item>Mentions and Replies</Breadcrumb.Item>}
+    >
       {posts.length > 0
         ? posts.map(p => <Post id={p.id} bubble={!p.read} key={p.id} />)
         : <p>You don't have any notifications just yet!</p>}
