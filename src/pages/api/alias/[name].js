17771fd19900b3d021d9f866f6f966ed370af004
Archie Baer <archie@abaer.dev>
2020-08-18 18:23:08 +0100

    Alias 404 response

--- src/pages/api/alias/[name].js
+++ src/pages/api/alias/[name].js
@@ -4,11 +4,14 @@ export default async (req, res) => {
   if (typeof req.query.name !== "string")
     return res.status(400).json({ err: "badRequest" });
 
-  res.json(
-    await db.Alias.findOne({
-      where: {
-        name: req.query.name,
-      },
-    })
-  );
+  // Get Alias
+  const alias = await db.Alias.findOne({
+    where: {
+      name: req.query.name,
+    },
+  });
+
+  // Response
+  if (alias) res.json(alias);
+  else res.status(404).json({ err: "notFound" });
 };
