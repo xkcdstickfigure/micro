af2e24015f3264079a2a959a75f7f28fcfd71b7c
Archie Baer <archie@abaer.dev>
2020-08-19 02:40:28 +0100

    Followers/Following endpoints

---
+++ src/pages/api/followers.js
@@ -0,0 +1,42 @@
+import db from "../../db";
+import auth from "../../utils/auth";
+import getUser from "../../utils/getUser";
+
+export default async (req, res) => {
+  const user = await auth(req.cookies.sessionToken);
+  if (!user) return res.status(401).send({ err: "badAuthorization" });
+
+  // Response
+  res.json({
+    count: await db.Follower.count({
+      where: {
+        following: user.id,
+      },
+    }),
+    users: (
+      await Promise.all(
+        (
+          await db.Follower.findAll({
+            where: {
+              following: user.id,
+            },
+            attributes: ["user"],
+            order: [["createdAt", "DESC"]],
+            limit: 100,
+          })
+        ).map(async (f) => {
+          const u = await getUser(f.user);
+          return (
+            u && {
+              id: u.user.id,
+              name: u.user.name,
+              tag: u.alles ? u.user.tag : "0000",
+              alles: u.alles,
+              avatar: u.user.avatar,
+            }
+          );
+        })
+      )
+    ).filter((f) => !!f),
+  });
+};
