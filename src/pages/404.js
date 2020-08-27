b7c9ec440688f36077c8ff4c41b5c0f60e6d7e4c
Archie Baer <archie@abaer.dev>
2020-08-28 00:44:27 +0100

    Improved 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,14 +5,16 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <Page>
-      <h1 className="text-5xl font-medium">404</h1>
-      <p>
-        We're having trouble finding that page! Maybe go to the{" "}
-        <Link href="/">
-          <a className="text-primary">homepage</a>
-        </Link>
-        .
-      </p>
+      <div>
+        <h1 className="text-5xl font-medium">404</h1>
+        <p>
+          We're having trouble finding that page! Maybe go to the{" "}
+          <Link href="/">
+            <a className="text-primary">homepage</a>
+          </Link>
+          .
+        </p>
+      </div>
 
       <ConfusedCat />
       <p className="text-right text-sm">
