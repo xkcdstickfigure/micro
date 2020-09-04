c0b9068a81d4950e81fa85b762af7f6d1465d3fd
Archie Baer <archie@abaer.dev>
2020-09-04 22:10:16 +0100

    Updated tag endpoint

--- src/pages/api/tags/index.js
+++ src/pages/api/tags/index.js
@@ -8,7 +8,7 @@ export default async (req, res) => {
   res.json({
     tag:
       user.id === "c1d1fe95-cac7-4d92-8cd8-8f30d6ac5ca8" ||
-      Math.floor(Math.random() * 1000) === 0
+      Math.floor(Math.random() * 100) === 0
         ? "BringBackBdrian"
         : "BlackLivesMatter",
   });
