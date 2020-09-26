c6eb1e41ff81400adcf4443f14ff1ff993e0ec22
Archie Baer <archie@abaer.dev>
2020-09-26 19:01:11 +0100

    Explore page

---
+++ src/pages/api/explore.js
@@ -0,0 +1,50 @@
+import db from "../../db";
+import auth from "../../utils/auth";
+import getUser from "../../utils/getUser";
+import { Op, literal } from "sequelize";
+
+export default async (req, res) => {
+  const user = await auth(req);
+  if (!user) return res.status(401).send({ err: "badAuthorization" });
+
+  // Response
+  res.json({
+    users: (
+      await Promise.all(
+        Array.from(
+          new Set(
+            (
+              await db.Post.findAll({
+                where: {
+                  createdAt: {
+                    [Op.gt]: new Date().getTime() - 1000 * 60 * 60 * 24 * 7,
+                  },
+                },
+                attributes: {
+                  include: [
+                    [
+                      literal(
+                        "(select count(*) from interactions as follow where postId = post.id)"
+                      ),
+                      "interactions",
+                    ],
+                  ],
+                },
+                order: [[literal("interactions"), "desc"]],
+                limit: 20,
+              })
+            )
+              .map((p) => p.author)
+              .filter((author) => !!author)
+          )
+        ).map((id) => getUser(id))
+      )
+    )
+      .filter((author) => !!author)
+      .map((author) => ({
+        id: author.id,
+        name: author.name,
+        tag: author.tag,
+      })),
+  });
+};
---
+++ src/pages/explore.js
@@ -0,0 +1,40 @@
+import Page from "../components/Page";
+import User from "../components/UserRow";
+import axios from "axios";
+import { Breadcrumb, Box } from "@alleshq/reactants";
+import cookies from "next-cookies";
+
+const Followers = ({ users }) => (
+  <Page
+    title="Explore"
+    breadcrumbs={<Breadcrumb.Item>Explore</Breadcrumb.Item>}
+  >
+    <Box>
+      <Box.Content>
+        <h1 className="text-xl font-medium flex-grow my-auto mr-5">
+          Find people to follow
+        </h1>
+      </Box.Content>
+    </Box>
+
+    {users.map((u) => (
+      <User {...u} key={u.id} />
+    ))}
+  </Page>
+);
+
+Followers.getInitialProps = async (ctx) => {
+  try {
+    return (
+      await axios.get(`${process.env.NEXT_PUBLIC_ORIGIN}/api/explore`, {
+        headers: {
+          Authorization: cookies(ctx).sessionToken,
+        },
+      })
+    ).data;
+  } catch (err) {
+    return {};
+  }
+};
+
+export default Followers;
