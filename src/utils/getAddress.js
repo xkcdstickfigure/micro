62e180b96357fb4ebb75a4d73127410bb78a76e0
Archie Baer <archie@abaer.dev>
2020-10-07 20:11:00 +0100

    Removed address util

--- src/utils/getAddress.js
+++
@@ -1,6 +0,0 @@
-export default function getAddress({ headers, connection }) {
-  if (headers["x-forwarded-for"]) {
-    const ips = headers["x-forwarded-for"].split(", ");
-    return ips[ips.length - 1];
-  } else return connection.remoteAddress;
-}
